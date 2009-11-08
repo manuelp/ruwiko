988f9321dc844216478133cd0558770fcfde8212
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2009-11-08 16:07:57 +0100

    First base version, fully functional.

---
+++ compile.rb
@@ -0,0 +1,59 @@
+require 'wiki_creole'
+require 'erb'
+
+HTML_DIR = "html"
+
+# Delete recursively the compile directory
+def delete_dir dir_name
+  puts "Deleting #{dir_name}..."
+  files = Dir.entries(HTML_DIR)
+  files.shift(2) # Remove "." and ".." from the files' list
+  files.each {|file| File.delete(File.join(HTML_DIR, file))}
+  Dir.delete(HTML_DIR)
+end
+
+def copy_css(data_dir, compile_dir)
+  files = Dir.glob("#{data_dir}#{File::SEPARATOR}*.css")
+  files.each {|file| file.sub!(/#{data_dir}#{File::SEPARATOR}/, '')}
+  
+  files.each do |file|
+    in_file = File.open("#{data_dir}#{File::SEPARATOR}#{file}", 'r')
+    out_file = File.open("#{compile_dir}#{File::SEPARATOR}#{file}", 'w')
+    out_file.write(in_file.read)
+    in_file.close
+    out_file.close
+  end
+end
+
+# Compile with WikiCreole every *.wiki file in the current directory 
+# and place the compiled files in the compile_dir directory.
+def compile_wiki_file(filename, compile_dir)
+  File.open(filename) do |file|
+    wiki_file = file.read
+    File.open(File.join(HTML_DIR, filename.sub(/\.wiki$/, "")), 'w') do |out_file|
+      content = WikiCreole.creole_parse(wiki_file)
+      html_file = ''
+      File.open(File.join('data', 'default.html')) {|file| html_file = file.read}
+      
+      erb = ERB.new(html_file)
+      html_file = erb.result(binding)
+      
+      out_file.puts(html_file)
+    end
+  end
+end
+
+#### MAIN ####
+
+delete_dir(HTML_DIR) if Dir.exists?(HTML_DIR)
+Dir.mkdir(HTML_DIR)
+copy_css('data', HTML_DIR)
+
+# For each *.wiki file, compile it
+dir = Dir.new('.')
+dir.each do |file| # TODO Usare Dir#glob
+  if file =~ /.*wiki$/
+    puts "Compiling #{file} -> #{HTML_DIR}"
+    compile_wiki_file(file, HTML_DIR)
+  end
+end
